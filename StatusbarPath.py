6ccfa1ad8afb8cba0c48ce9b8f5098c8441eb3b7
Steven Lu <stevenlu443@gmail.com>
2013-04-25 11:46:36 -0400

    htat last change did not do anything (wrong version)

--- StatusbarPath.py
+++ StatusbarPath.py
@@ -6,5 +6,9 @@ class CurrentPathStatusCommand(sublime_plugin.EventListener):
 
     def on_activated(self, view):
         filename = view.file_name()
-        if filename and view.isFullScreen():
+        if filename:
+            print "SSP: "
+            print dir(view)
+            print "Window: "
+            print dir(view.window())
             view.set_status('Path', filename)
