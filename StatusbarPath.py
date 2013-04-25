62a165b38a531f3c3bb265f958cc73f2ca3f03f2
Steven Lu <stevenlu443@gmail.com>
2013-04-25 11:28:38 -0400

    Adding the condition to only show when in fullscreen mode

--- StatusbarPath.py
+++ StatusbarPath.py
@@ -6,5 +6,5 @@ class CurrentPathStatusCommand(sublime_plugin.EventListener):
 
     def on_activated(self, view):
         filename = view.file_name()
-        if filename:
+        if filename and view.isFullScreen():
             view.set_status('Path', filename)
