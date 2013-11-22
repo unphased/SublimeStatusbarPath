8a7651fa924bd0b567ae4c50452baf4ab72e6d92
Joe Tanner <joe.j.tanner@gmail.com>
2013-11-22 00:03:24 -0700

    shorten the path by replacing the path of the project folder with '.'
    e.g. /Users/bob/src/myproject/somedir/foo.txt with be ./somedir/foo.txt

--- StatusbarPath.py
+++ StatusbarPath.py
@@ -7,4 +7,6 @@ class CurrentPathStatusCommand(sublime_plugin.EventListener):
     def on_activated(self, view):
         filename = view.file_name()
         if filename:
+            for folder in view.window().folders():
+                filename = filename.replace(folder, '.')
             view.set_status('zPath', filename)
